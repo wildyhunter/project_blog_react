b8fda8a994bfbc79c1ef1a4f5d7b412812a6f098
VAGNER OLIVEIRA <116770973+wildyhunter@users.noreply.github.com>
2024-08-15 14:18:40 +0100

    feat: add new routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Children, StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+import Home from './routes/Home.jsx';
+import NewPost from './routes/NewPost.jsx';
+
+const router = createBrowserRouter([
+    {
+        Element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            {
+                path: '/new',
+                element: <NewPost />,
+            },
+        ],
+    },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
